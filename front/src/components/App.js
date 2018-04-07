03ef4f0c8e17be996cb2f7412ae48890823d9436
EC2 Default User <ec2-user@ip-172-31-19-38.eu-west-1.compute.internal>
2018-04-07 22:54:11 +0000

    Create Help Seeker path, Step1, Step2

--- front/src/components/App.js
+++ front/src/components/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import HomeHelpSeeker from './HomeHelpSeeker';
 import HelpSeekerNeedsStep1 from './HelpSeekerNeeds-Step1';
 import HelpSeekerNeedsStep2 from './HelpSeekerNeeds-Step2';
+import HelpSeekerNeedsStep3 from './HelpSeekerNeeds-Step3';
 
 class App extends Component {
   render() {
@@ -15,6 +16,7 @@ class App extends Component {
             <Route path="/" exact component={HomeHelpSeeker} />
             <Route path="/HelpSeeker/Step1" exact component={HelpSeekerNeedsStep1} />
             <Route path="/HelpSeeker/Step2" exact component={HelpSeekerNeedsStep2} />
+            <Route path="/HelpSeeker/Step3" exact component={HelpSeekerNeedsStep3} />
           </Switch>
         </Router>
       </div>
