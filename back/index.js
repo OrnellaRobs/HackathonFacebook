c12367690ac5ede4a02259ac40daa8c671983383
victor <victor.four@gmail.com>
2018-04-08 04:18:14 +0200

    lollll

--- back/index.js
+++ back/index.js
@@ -3,6 +3,7 @@ var server = require('http').createServer(app)
 var io = require('socket.io')(server)
 
 var helper_status = false;
+var help_needed = false;
 
 app.get('/helper_call', (req, res) => {
 	res.set('Access-Control-Allow-Origin', '*');
@@ -10,7 +11,19 @@ app.get('/helper_call', (req, res) => {
 })
 
 app.get('/helper_ready', (req, res) => {
+	res.set('Access-Control-Allow-Origin', '*');
 	helper_status = true;
+	res.send('truc');
+})
+
+app.get('/help_needed', (req, res) => {
+	res.set('Access-Control-Allow-Origin', '*');
+	res.send({"status":help_needed});
+})
+
+app.get('/help_request', (req, res) => {
+	res.set('Access-Control-Allow-Origin', '*');
+	help_needed = true;
 	res.send('truc');
 })
 
