31f8b0a6f881ff82f7a326353968c0a4e035d59f
victor <victor.four@gmail.com>
2018-04-08 03:31:49 +0200

    simple test

--- back/index.js
+++ back/index.js
@@ -2,244 +2,30 @@ var app = require('express')();
 var server = require('http').createServer(app)
 var io = require('socket.io')(server)
 
-app.get('/seeker', (req, res) => {
-	res.sendFile(__dirname + '/seeker.html')
-})
-
-app.get('/helper', (req, res) => {
-	res.sendFile(__dirname + '/helper.html')
-})
-
-var helpers = [
-	{
-		"id" : 0,
-		"firstname" : "Léna",
-		"lastname" : "Giacalone",
-		"ETA" : "42 min",
-		"photo" : "https://cdn.intra.42.fr/users/medium_lgiacalo.jpg"
-	},
-	{
-		"id" : 4,
-		"firstname" : "Victor",
-		"lastname" : "Four",
-		"ETA" : "42 min",
-		"photo" : "https://cdn.intra.42.fr/users/medium_vfour.jpg"
-	},
-	{
-		"id" : 2,
-		"firstname" : "Martin",
-		"lastname" : "Hammerchmidt",
-		"ETA" : "42 min",
-		"photo" : "https://cdn.intra.42.fr/users/medium_mhammerc.jpg"
-	},
-	{
-		"id" : 3,
-		"firstname" : "Ornella",
-		"lastname" : "Razafinony Robinson",
-		"ETA" : "42 min",
-		"photo" : "https://cdn.intra.42.fr/users/medium_orazafin.jpg"
-	}
-]
-
-var seekers = [
-	{
-		"id" : 1,
-		"firstname" : "Lara",
-		"lastname" : "Ramirez",
-		"ETA" : "42 min",
-		"photo" : "https://cdn.intra.42.fr/users/medium_lramirez.jpg"
-	}
-]
-
-
-var available_helpers = []
-
-var seeker_i = 0;
-var helper_i = 0;
-
-function findHelpers(helpers, id_list){
-	var res = [];
-	var i = 0;
-	for(helper of helpers){
-		var helper_index = id_list.indexOf(helper.id);
-		if(helper_index != -1){
-			res.push(i);
-		}
-		i++;
-	}
-	return res;
-}
 
 io.sockets.on('connection', (socket) => {
-	console.log('a user connected')
-	socket.on('whoami-seeker', () => {
-		console.log('whoami-seeker');
-		socket.emit('whoami-seeker', seekers[0]);
-	});
-
-	socket.on('whoami-helper', () => {
-		helpers[helper_i]["socket"] = socket.id;
-		socket.emit('whoami-helper', helpers[helper_i++]);
-		helper_i = helper_i % helpers.length;
-	});
-
 	socket.on('seeker', (req) => {
 		switch(req.category) {
-			case 'list_helpers':
-				socket.emit('seeker', {"category":"list_helpers", "helpers":helpers});
-				break;
 			case 'call_helpers':
-				var truc = req;
-				console.log(findHelpers(helpers, req.helpers));
-				for(helper_id of findHelpers(helpers, req.helpers)){
-					io.sockets.connected[helpers[helper_id].socket].emit("helper", truc);
-				}
+				//socket.emit('helper', req);
+				socket.emit('seeker', 'hello');
+				break;
+			case 'cancel_request':
+				//send all helpers that help is no longer needed
 				break;
 		}
 	})
 
-	//socket.on('helper-accept', (req) => {
+	socket.on('helper', (req) => {
+		switch(req.category) {
+			case 'helper_answer':
+				if(req.answer == 'yep'){
+					//signal other helpers that seeker found help
+				} else {
+					//remove helper from helper list
+					//if there is no helpers in the helper list, send seeker that they are fucked
+				}
+				break;
+		}
+	});
 });
-
-/*
-req:
-{
-	"category": "call_helpers",
-	"seeker": {
-		"firstname" : "truc",
-		"lastname" : "machin",
-		"photo" : "https://cdn.intra.42.fr/users/medium_lramirez.jpg"
-		"location": {}
-	},
-	"helpers" : [0,3],
-	"problem":{
-		"help_type" : "truc",
-		"description": "my problem",
-		"photo": base64encoded_photo,
-	}
-}
-//request helpers
-{
-	"categoy": "seek_help",
-	"seeker": {
-		"firstname" : "Lara",
-		"lastname" : "Ramirez",
-		"photo" : "https://cdn.intra.42.fr/users/medium_lramirez.jpg"
-		"location": {}
-	},
-	"problem":{
-		"help_type" : "truc",
-		"description": "my problem",
-		"photo": base64encoded_photo,
-	}
-}
-
-*/
-server.listen(3000, () => console.log('Example app listening on port 3000!'))
-
-
-/*
-http:
-/ : envoyer le html + le js react
-
-websockets:
-req:
-{
-	"category": "list_helpers"
-	//"geoloc_seeker" : lon, lat
-}
-res:
-{
-	"category": "list_helpers",
-	"helpers":[
-		{
-			"id": "id",
-			"firstname" : "truc",
-			"lastname" : "machin",
-			"ETA" : "42 min",
-			"photo" : base64encoded_photo,
-		}
-		//....
-	]
-}
-
-//coté client on choisit les helpers
-req:
-{
-	"category": "call_helpers",
-	"seeker": {
-		"id": "id",
-		"firstname" : "truc",
-		"lastname" : "machin",
-		"ETA" : "42 min",
-		"photo" : base64encoded_photo,
-		"location": {
-			"lon": lon,
-			"lat": lat
-		}
-	},
-	"helpers" : [id1, id2, id3],
-	"problem":{
-		"help_type" : "truc",
-		"description": "my problem",
-		"photo": base64encoded_photo,
-	}
-}
->>> requete vers helpers
-res:
-{
-	"category": "call_helpers",
-	"helper": {
-		"id": "id",
-		"firstname" : "truc",
-		"lastname" : "machin",
-		"ETA" : "42 min",
-		"photo" : base64encoded_photo,
-		"messenger_login": trucmachin,
-		"location": {
-			"lon": lon,
-			"lat": lat
-		}
-	}
-}
-
-
-//requete vers helper
-req:
-{
-	"categoy": "seek_help",
-	"seeker": {
-		"id": "id",
-		"firstname" : "truc",
-		"lastname" : "machin",
-		"ETA" : "42 min",
-		"photo" : base64encoded_photo,
-		"location": {
-			"lon": lon,
-			"lat": lat
-		}
-	},
-	"problem":{
-		"help_type" : "truc",
-		"description": "my problem",
-		"photo": base64encoded_photo,
-	}
-}
-res:
-{
-	"category": "seek_help",
-	"accept": true,
-	"helper": {
-		"id": "id",
-		"firstname" : "truc",
-		"lastname" : "machin",
-		"ETA" : "42 min",
-		"photo" : base64encoded_photo,
-		"messenger_login": trucmachin,
-		"location": {
-			"lon": lon,
-			"lat": lat
-		}
-	}
-}
-*/
